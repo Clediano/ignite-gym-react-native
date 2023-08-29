286b82440093b4183c4142362b7ac4ad42c3b4ca
CLEDIANO ESTEFENON <clediano.estefenon@capgemini.com>
2023-08-29 09:36:35 -0300

    adicionado history page

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,9 +1,40 @@
-import { Center, Text } from "native-base";
+import { Heading, VStack, SectionList, Box, Text } from "native-base";
+
+import { ScreenHeader } from "@components/ScreenHeader";
+import { HistoryCard } from "@components/HistoryCard";
+import { useState } from "react";
 
 export function History() {
-    return (
-        <Center flex={1}>
-            <Text>History</Text>
-        </Center>
-    );
+  const [exercises, setExercises] = useState([
+    { title: "28.08.2023", data: ["Puxada frontal", "Supino com halteres"] },
+    { title: "27.08.2023", data: ["Agachamento livre", "Supino reto"] },
+  ]);
+
+  return (
+    <VStack flex={1}>
+      <ScreenHeader title="Histórico de Exercícios" />
+
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <HistoryCard />}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" mt={5} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        contentContainerStyle={
+          exercises.length === 0 && { flex: 1, justifyContent: "center" }
+        }
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text color="gray.100" textAlign="center">
+            Não há exercícios registrados ainda {"\n"}
+            Bora treinar?
+          </Text>
+        )}
+        px={8}
+      />
+    </VStack>
+  );
 }
